dcc63be4c5f69a6afa1e28aaf61aa2a3e458337a
ritu <ritu20033005@gmail.com>
2023-07-04 17:42:32 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed =new Typed(".typing", {
+    strings: ["Developer", "Designer", "Freelancer", "Front end developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay:1000,
+    loop: true
+});
